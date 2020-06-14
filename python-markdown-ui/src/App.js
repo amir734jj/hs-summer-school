00fcd4f7c3dddd6cbac52510019149b5db7bdbc8
Amir <hesamian@uwm.edu>
2020-06-13 22:17:31 -0500

    added ui

--- python-markdown-ui/src/App.js
+++ python-markdown-ui/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      html: '',
+    };
+  }
+
+  getHtml = async event => {
+    const markdown = event.target.value;
+    const response = await axios.post('http://localhost:5000/md', { markdown });
+    this.setState({ html: response.data });
+  }
+
+  render() {
+    return (
+      <div className='container-fluid' >
+        <h3>python - markdown </h3>
+        <div className='container' >
+          <div className='row' >
+            <div className='col-sm-12' >
+              <textarea className={'form-control'} onChange={this.getHtml} rows='10'></textarea>
+            </div>
+            <div className='col-sm-12'>
+              <hr />
+            </div>
+            <div className='col-sm-12' dangerouslySetInnerHTML={{ __html: this.state.html }}>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
