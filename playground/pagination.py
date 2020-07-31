f5a9e57349bb77e08ebd25e5349e79ff2fb894f9
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-07-30 20:14:11 -0500

    Update pagination.py

--- playground/pagination.py
+++ playground/pagination.py
@@ -1,5 +1,34 @@
+def in_range(lower, upper, n):
+    return n >= lower and n <= upper
+
 def get_available_pages(page_number, pages, window_size):
-    return [1,2,3,4,5]
+
+    result = [None] * window_size
+    
+    # page_number = 6
+    # pages       = 10
+    # window_size = 5
+    # [ 0, 1, 2, 3, _ ]
+
+    i = 0
+    size = 0
+    while size < window_size:
+        lower_index_candidate = page_number - i - 1
+        upper_index_candidate = page_number + i - 1
+    
+    
+        if in_range(0, window_size - 1, lower_index_candidate) and result[lower_index_candidate] is None:
+            result[lower_index_candidate] = page_number - i
+            size = size + 1
+        if in_range(0, window_size - 1, upper_index_candidate) and result[upper_index_candidate] is None:
+            result[upper_index_candidate] = page_number + i
+            size = size + 1
+
+        print(f" {i} => {result}")
+
+        i = i + 1
+
+    return result
 
 
 def assert_thing(expected, received):
